c5f49db8d5b0f4be9324ca58466d19f985db9a36
Sumit Kumar Roy <priencraj32@gmail.com>
2025-06-04 12:14:42 +0530

    complete po item tab

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,9 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
-`@objc class AppDelegate: FlutterAppDelegate {
-`  override func application(
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
